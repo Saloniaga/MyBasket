073d36f8adb2caf8aa86d193c884453736f4acd1
Saloniaga <sagarwal9475@gmail.com>
2022-06-12 22:28:10 +0530

    MyBasket

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>MY BASKET</h1>
-      <Basket text="fruits" />
+      <Basket text="fruit" />
       <Basket text="dairy" />
       <Basket text="nuts" />
     </div>
